0ddc7eb30545f0f9bd3cbff702152cc2ce308c84
Pablo <pablocd168@gmail.com>
2022-02-12 17:11:42 +0100

    First commit

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+	env: {
+		browser: true,
+		es2021: true
+	},
+	settings: {
+		react: {
+			version: 'detect'
+		}
+	},
+	extends: [
+		'plugin:react/recommended',
+		'plugin:react/jsx-runtime',
+		'standard',
+		'eslint-config-prettier'
+	],
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true
+		},
+		ecmaVersion: 'latest',
+		sourceType: 'module'
+	},
+	plugins: ['react'],
+	rules: {}
+};
---
+++ src/main.jsx
@@ -0,0 +1,7 @@
+import ReactDOM from 'react-dom';
+import './index.css';
+
+const app = <img src='/logo.svg' />;
+const container = document.getElementById('root');
+
+ReactDOM.render(app, container);
